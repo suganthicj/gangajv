c548e99096d611d4ee46074d27ad69bf9f73df38
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-15 21:35:07 -0700

    Create gangajv.py

---
+++ gangajv.py
@@ -0,0 +1,8 @@
+def convert(seconds): 
+    min, sec = divmod(seconds, 60) 
+    hour, min = divmod(min, 60) 
+    return "%d:%02d:%02d" % (hour, min, sec) 
+      
+# Driver program 
+n = 12345
+print(convert(n)) 
